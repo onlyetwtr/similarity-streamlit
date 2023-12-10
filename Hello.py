510e82b244e940155ba724542e5cf746f5341c8a
onlyetwtr <63968524+onlyetwtr@users.noreply.github.com>
2023-12-10 16:20:15 -0500

    Update Hello.py to real app

--- Hello.py
+++ Hello.py
@@ -1,50 +1,85 @@
-# Copyright 2018-2022 Streamlit Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 import streamlit as st
-from streamlit.logger import get_logger
+from haystack.pipelines import Pipeline
+from haystack.nodes import EmbeddingRetriever
+import pandas as pd
+import joblib
 
-LOGGER = get_logger(__name__)
-
-def run():
-    st.set_page_config(
-        page_title="Hello",
-        page_icon="👋",
-    )
-
-    st.write("# Welcome to Streamlit! 👋")
-
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
+# Function to load the retriever and document store
+def load_resources():
+    if "document_store" not in st.session_state or "retriever" not in st.session_state:
+        st.session_state.document_store = joblib.load("doc_str_joblib.joblib")
+        st.session_state.retriever = EmbeddingRetriever(
+            document_store=st.session_state.document_store, 
+            embedding_model="BAAI/bge-large-en"
+        )
+        st.session_state.pipeline = Pipeline()
+        st.session_state.pipeline.add_node(component=st.session_state.retriever, name="retriever", inputs=["Query"])
 
 
+st.session_state.df = pd.read_csv("processed_text.csv")
+
+
+
+# Streamlit interface
+
+def update_output(query, slider_value):
+    result = st.session_state.pipeline.run(
+        query=query,
+        params={
+            "retriever": {"top_k": slider_value},
+        },
+    )  # "Reader": {"top_k": 1}})
+    catch_list = []
+    for pt_link_index in range(0,len(result['documents'])):
+        pt_link = result['documents'][pt_link_index].to_dict()['meta']['source']
+        text_for_output = st.session_state.df[st.session_state.df['patient_fix'] == pt_link]['raw_text'].iloc[0]
+        # text_for_output = text_for_output.replace('\n',' ')
+        catch_list.append(text_for_output)
+
+    warmups = [result.split('Warmup: ')[1].split('Cooldown: ')[0].replace(': ','') for result in catch_list]
+    cooldowns = [result.split('Cooldown: ')[1].replace(': ','') for result in catch_list]
+    exercises = [result.split('Exercises')[1].replace(': ','') for result in catch_list]
+
+    zipped_list = list(zip(warmups, cooldowns, exercises))
+
+    # for i in range(0,slider_value):
+    #     print('query for search',query,'\n')
+    #     print('returned results:')
+    #     print('warmups:',zipped_list[i][0],'\n')    
+    #     print('cooldowns:', zipped_list[i][1],'\n')
+    #     print('exercises:', zipped_list[i][2],'\n')
+    out_df = pd.DataFrame(zipped_list, 
+                columns=['warmups', 'cooldowns' , 'exercises'])
+
+    # # The output for each section is simply the slider value
+    # # warmup = ['warmup1','warmup2']
+    # # cooldown = ['cooldown1', 'cooldown2']
+    # # exercises = ['exercise1', 'exercise2']
+    # # return warmup, cooldown, exercises
+    # result1 = ["result1", "result1", "result1", "result1"]
+    # result2 = ["result2", "result2", "result2", "result2"]
+    # df = pd.DataFrame(
+    #     [result1, result2], columns=["rank", "warmup", "cooldown", "exercises"]
+    # )
+
+    return out_df
+
+# Streamlit main body
 if __name__ == "__main__":
-    run()
+    st.title("UMICH Similarity App")
+    
+    load_resources()
+    
+    
+    with st.form("my_form"):
+        query = st.text_input("Screening Findings")
+        slider_value = st.slider("Results to match", 1, 5, 1)
+
+        # Every form must have a submit button.
+        submitted = st.form_submit_button("Search")
+        if submitted:
+            results = update_output(query, slider_value)
+            st.dataframe(results)
+
+
+# No need for a separate launch command
